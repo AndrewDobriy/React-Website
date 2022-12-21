9c359388e22eee9d575e1c66eb4661876c5233cc
AndrewDobriy <an.dobriy@mail.ru>
2022-12-21 15:57:25 +0300

    add styles for cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.js';
+import Services from './components/pages/Services.js';
+import Products from './components/pages/Products.js';
+import SignUp from './components/pages/SignUp.js';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
