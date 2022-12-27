20947c51fc0d8951b8d4756c138d5b90835788ad
AndrewDobriy <an.dobriy@mail.ru>
2022-12-27 22:16:45 +0300

    add support safari

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,14 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="React-Website/videos/video-1.mp4" autoPlay loop muted />
+      <video
+        src="React-Website/videos/video-1.mp4"
+        autoPlay
+        loop
+        muted
+        playsinline="true"
+        disablePictureInPicture="true"
+      />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for ?</p>
       <div className="hero-btns">
